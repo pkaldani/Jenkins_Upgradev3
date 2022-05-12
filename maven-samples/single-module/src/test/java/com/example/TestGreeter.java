4944816c7419d2d0d93ac127c95bac206c21d8a4
Paata Kaldani <43200375+pkaldani@users.noreply.github.com>
2022-05-12 23:59:20 +0400

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -36,7 +36,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "Hello World, This is Paata Kaldani";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
